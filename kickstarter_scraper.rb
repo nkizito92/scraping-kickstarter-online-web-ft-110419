7eb6624b9171d8987a2073f77e4a54a0386e621b
Kizito Njoku <nkizito57@gmail.com>
2019-11-25 02:22:21 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,4 +12,6 @@ end
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text 
 # locatoin: project.css("ul.project-stats li.first.funded strong").text
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+
 create_project_hash
